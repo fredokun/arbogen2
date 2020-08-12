3f08d8fd7e2d5e0a61bef3b839d761483ddc01ec
Frederic Peschanski <fredokun@github.com>
2020-08-12 18:28:59 +0200

    norm_dist

--- src/utils.rs
+++ src/utils.rs
@@ -15,3 +15,20 @@ pub fn norm_dist(v1 : &Values, v2 : &Values) -> f64 {
     return dist;
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_norm_dist() {
+	// (norm {:a 0.1 :b 0.3} {:a 0.2 :b -0.2})
+	let mut v1 = HashMap::new();
+	v1.insert("a".to_string(), 0.1);
+	v1.insert("b".to_string(), 0.3);
+	let mut v2 = HashMap::new();
+	v2.insert("a".to_string(), 0.2);
+	v2.insert("b".to_string(), -0.2);
+	assert_eq!(norm_dist(&v1, &v2), 0.5);
+    }
+
+}
